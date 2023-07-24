c1f6d7118449ac0249d47c9314dadeb7e27d66a8
doublekm153 <doublekm153@gmail.com>
2023-07-25 01:21:04 +0900

    LoginScreen.css_v1

--- src/userManagament/loginScreen/LoginScreen.jsx
+++ src/userManagament/loginScreen/LoginScreen.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Link, useNavigate} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie"
 import "./LoginScreen.css"
 
@@ -8,8 +8,8 @@ function LoginScreen() {
     const [cookies, setCookie, removeCookie] = useCookies(['userDate']);
     const navi = useNavigate();
     const [form, setForm] = useState({
-        user_Id : "",
-        user_Pw : ""
+        user_Id: "",
+        user_Pw: ""
     });
 
     const onChange = (e) => {
@@ -21,44 +21,42 @@ function LoginScreen() {
     }
     const loginConstraints = () => {
         fetch("http://localhost:8080/accounts/login", {
-            method : "GET",
-            headers : {
-                "Content-Type" : "application/json; charset=utf-8"
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
             },
             body: JSON.stringify(form)
         }).then(res => res.json())
             .then(res => {
-                if(res.user_Id === form.user_Id){
+                if (res.user_Id === form.user_Id) {
                     setCookie('userDate', {
-                        user_NickName : res.user_NickName,
-                        user_Id : res.user_Id,
-                        user_Pw : res.user_Pw
+                        user_NickName: res.user_NickName,
+                        user_Id: res.user_Id,
+                        user_Pw: res.user_Pw
                     })
                     navi("/")
                 }
             })
-            .catch( err =>
+            .catch(err =>
                 alert("로그인에 실패 하였습니다.")
             )
     }
-
-
     return (
         <div className={"loginScreen"}>
-            <div>
-                <input value={form.user_Id}
-                       onChange={(e) => onChange(e)}
-                       placeholder={"id"}/>
+            <div className="inputContainer">
+                <input value={form.userId}
+                    onChange={(e) => onChange(e)}
+                    placeholder={"아이디를 입력하세요"} />
                 <input value={form.userPw}
-                       onChange={(e) => onChange(e)}
-                       placeholder={"pw"}/>
-                <button onClick={() => loginConstraints()}>로그인</button>
+                    onChange={(e) => onChange(e)}
+                    placeholder={"비밀번호를 입력하세요"} />
             </div>
-            <div style={{marginTop: 10}}>
+            <div className="buttonContainer">
+                <button onClick={() => loginConstraints()} className="loginButton">로그인</button>
                 <Link to={'/loginPage/signUpPage'}>
-                    <button>회원가입</button>
+                    <button className="signupButton">회원가입</button>
                 </Link>
-                <button>id/pw 찾기</button>
+                <button className="findIdPwButton">id/pw 찾기</button>
             </div>
         </div>
     );
