62ad6f35ff524d4a7eba29ade7858b6ec710fcaa
wlsn0105 <jack13579@naver.com>
2023-07-25 14:49:59 +0900

    fix

--- src/mainScreen/Header.jsx
+++ src/mainScreen/Header.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import "./Header.css"
+import mainImage from '../img/main.jpg';
+
+/*
+임시 main.jpg 추가
+return문 안에
+main-container 추가
+maingImage 추가
+BodyRightComponents.jsx의 title과 id값 겹침
+*/
+
+/*
+이미지 높이 값 780px로 수정
+title id > title-main으로 수정
+title-main안에 mainPhototext 추가
+*/
 
 function Header(props) {
 
 
     return (
         <div className={"header"}>
-            <div className="title">
-                SALT
+            <div className={"main-container"}>
+                <img className="mainImage" alt="main" src={mainImage} width="100%" height="780px"/>
             </div>
+            <div className="title-main">
+                <div className="mainPhototext">
+                개발, AI 인공지능, 보안
+                </div>
+                <div className="mainPhototext">
+                소프트웨어에 대한 모든 것
+                </div>
+            </div>
+            
         </div>
     );
 }
--- src/mainScreen/MainScreen.jsx
+++ src/mainScreen/MainScreen.jsx
@@ -5,18 +5,26 @@ import BodyRightComponents from "./components-right/BodyRightComponents";
 import Footer from "./Footer";
 import "./MainScreen.css"
 
+/*
+return문 안에
+mainPhoto 객체 추가
+Header를 mainScreen > mainPhoto로 위치 변경
+*/
 
 function MainScreen(props) {
 
 
     return (
-        <div className={"mainScreen"}>
+        <div className={"mainPhoto"}>
             <Header />
+        <div className={"mainScreen"}>
+            
             <div className={"body"}>
                 <BodyLeftComponents />
                 <BodyRightComponents />
             </div>
             <Footer />
+        </div>
         </div>
     );
 }
