85a03cc1e60010a9baf49a84e312d2e594f99d12
wlsn0105 <jack13579@naver.com>
2023-07-30 21:02:08 +0900

    메뉴1,2 delete
    blankbox delete
    menu-button > menu-option fix
    registerCheck 주석처리

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -5,16 +5,12 @@ import logoImage from '../img/logo2.png';
 import {useCookies} from "react-cookie";
 
 /* 
-logo2.png 추가 및 logo.png2로 변경
-logoImage height값 추가
+메뉴1,2 delete
+Register-button 링크경로 fix(loginScreen/signUpScreen > signUpScreen 처음이랑 동일)
+blankbox delete
+menu-button > menu-option fix
+registerCheck 주석처리
 */
-
-/*
-menu-list 추가
-위치 변경이 안되서 사이에 blankbox 임시 추가
-버튼 구분 위해 menu-button 추가
-*/
-
 
 function NavigationBar(props) {
 
@@ -25,14 +21,14 @@ function NavigationBar(props) {
             <button className="login-button">로그인</button>
         </Link>
     };
-    const registerCheck = () => {
-        return cookies.userDate ?
-            <button className="LoginOut-button"
-                     onClick={()  => removeCookie('userDate')}>로그아웃</button> :
-            <Link to="/loginScreen/signUpScreen">
-                <button className="Register-button">회원가입</button>
-            </Link>
-    };
+    // const registerCheck = () => {
+    //     return cookies.userDate ?
+    //         <button className="LoginOut-button"
+    //                  onClick={()  => removeCookie('userDate')}>로그아웃</button> :
+    //         <Link to="/signUpScreen">
+    //             <button className="Register-button">회원가입</button>
+    //         </Link>
+    // };
 
     return (
         <div className={"navigationBar"}>
@@ -46,13 +42,9 @@ function NavigationBar(props) {
                 <button className="menu-item">SAL</button>
                 <button className="menu-item">개발자들</button>
                 </div>
-                <div className="blankbox"></div>
-                <div className="menu-button">
-                
-                <button className="menu-item">메뉴1</button>
-                <button className="menu-item">메뉴2</button>
-                {cookies.userDate ? <p>{cookies.userDate.user_Id}</p> : loggedOut()}
-                {registerCheck()}
+                <div className="menu-option">
+                    {cookies.userDate ? <p>{cookies.userDate.user_Id}</p> : loggedOut()}
+                    {/* {registerCheck()} */}
                 </div>
             </div>
         </div>
