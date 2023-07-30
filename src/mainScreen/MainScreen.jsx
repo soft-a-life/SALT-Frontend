73268811ccd5e36d47acb712ded5c899b804f9f5
wlsn0105 <jack13579@naver.com>
2023-07-30 21:57:34 +0900

    mainPhoto fix
    위치 변경

--- src/mainScreen/MainScreen.jsx
+++ src/mainScreen/MainScreen.jsx
@@ -6,25 +6,25 @@ import Footer from "./Footer";
 import "./MainScreen.css"
 
 /*
-return문 안에
-mainPhoto 객체 추가
-Header를 mainScreen > mainPhoto로 위치 변경
-Footer mainScreen에서 위치 변경
+mainPhoto fix
+
+위치 변경
 */
 
 function MainScreen(props) {
 
     return (
-        <div className={"mainPhoto"}>
+        <div>
+            <div className={"mainPhoto"}>
             <Header />
-        <div className={"mainScreen"}>
-            
-            <div className={"body"}>
-                <BodyLeftComponents />
-                <BodyRightComponents />
             </div>
-        </div>
-        <Footer />
+            <div className={"mainScreen"}>
+                <div className={"body"}>
+                    <BodyLeftComponents />
+                    <BodyRightComponents />
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 }
