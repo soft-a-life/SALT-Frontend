d11e47700af330093523c8c3659e54286dc3227e
kimyougmin <dudals896@gmail.com>
2023-08-06 13:08:40 +0900

    add SearchIdPwScreen.jsx add Timer.jsx

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import LoginScreen from './userManagament/loginScreen/LoginScreen'
 import SignUpScreen from './userManagament/signupScreen/SignUpScreen'
 import PostWriteScreen from './PostWriteScreen/PostWriteScreen'
 import { useCookies } from 'react-cookie'
+import SearchIdPwScreen from './userManagament/searchIdPwScreen/SearchIdPwScreen'
 
 function App() {
   const [cookies, setCookie, removeCookie] = useCookies(['userDate'])
@@ -28,6 +29,10 @@ function App() {
           ></Route>
           <Route path="/loginScreen" exact element={<LoginScreen />}></Route>
           <Route path="/signUpScreen" element={<SignUpScreen />}></Route>
+          <Route
+            path="/searchIdPwScreen"
+            element={<SearchIdPwScreen />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Timer.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+
+// eslint-disable-next-line react/prop-types
+function Timer({ min, secon }) {
+  const [minutes, setMinutes] = useState(5)
+  const [seconds, setSeconds] = useState(0)
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1)
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(countdown)
+        } else {
+          setMinutes(minutes - 1)
+          setSeconds(59)
+        }
+      }
+    }, 1000)
+    return () => clearInterval(countdown)
+  }, [minutes, seconds])
+  return (
+    <div style={{ display: 'flex' }}>
+      0{minutes} : {seconds}
+    </div>
+  )
+}
+
+export default Timer
