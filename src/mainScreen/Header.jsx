d331a3bed59ebbcfdd25d79d134c2548362e1188
wlsn0105 <jack13579@naver.com>
2023-08-10 16:10:30 +0900

    Header img invisible function add

--- src/mainScreen/Header.jsx
+++ src/mainScreen/Header.jsx
@@ -1,37 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import mainImage from '../img/main.jpg'
 
-/*
-임시 main.jpg 추가
-return문 안에
-main-container 추가
-maingImage 추가
-BodyRightComponents.jsx의 title과 id값 겹침
-*/
-
-/*
-이미지 높이 값 780px로 수정
-title id > title-main으로 수정
-title-main안에 mainPhototext 추가
-title-main main-container 안으로 변경
-*/
-
 function Header(props) {
+  const [imageVisible, setImageVisible] = useState(true)
+
+  const removeImage = () => {
+    setImageVisible(false)
+  }
+
   return (
     <div className={'header'}>
       <div className={'main-container'}>
-        <img
-          className="mainImage"
-          alt="main"
-          src={mainImage}
-          width="100%"
-          height="780px"
-        />
-        <div className="title-main">
-          <div className="mainPhototext">개발, AI 인공지능, 보안</div>
-          <div className="mainPhototext">소프트웨어에 대한 모든 것</div>
-        </div>
+        {imageVisible && (
+          <div>
+            <img
+              className="mainImage"
+              alt="main"
+              src={mainImage}
+              width="100%"
+              height="780px"
+            />
+            <div className="hideImg">
+              <button onClick={removeImage}>이미지 없애기</button>
+            </div>
+            <div className="title-main">
+              <div className="mainPhototext">개발, AI 인공지능, 보안</div>
+              <div className="mainPhototext">소프트웨어에 대한 모든 것</div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
