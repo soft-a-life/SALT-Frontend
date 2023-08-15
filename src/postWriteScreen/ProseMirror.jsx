8ba3ef4943700a3fdba6f69e2d782b910638a3b2
wlsn0105 <jack13579@naver.com>
2023-08-15 11:01:02 +0900

    Fix: ProseMirror error

--- src/postWriteScreen/ProseMirror.jsx
+++ src/postWriteScreen/ProseMirror.jsx
@@ -1,33 +1,33 @@
-import { EditorState } from 'prosemirror-state'
-import { EditorView } from 'prosemirror-view'
-import { Schema, DOMParser } from 'prosemirror-model'
-import { schema } from 'prosemirror-schema-basic'
-import { addListNodes } from 'prosemirror-schema-list'
-import { exampleSetup } from 'prosemirror-example-setup'
-import { useEffect, useRef } from 'react'
-import './ProseMirror.css'
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import { Schema, DOMParser } from "prosemirror-model";
+import { schema } from "prosemirror-schema-basic";
+import { addListNodes } from "prosemirror-schema-list";
+import { exampleSetup } from "prosemirror-example-setup";
+import { useEffect, useRef } from "react";
+import "./ProseMirror.css"
 
 const mySchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
-  marks: schema.spec.marks,
-})
+    nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+    marks: schema.spec.marks
+});
 
-const doc = DOMParser.fromSchema(mySchema).parse(document.createElement('div'))
+const doc = DOMParser.fromSchema(mySchema).parse(document.createElement("div"));
 
-const plugins = exampleSetup({ schema: mySchema })
+const plugins = exampleSetup({ schema: mySchema });
 
 export function Editor() {
-  const editorRef = useRef(null)
-  const editorDom = useRef(null)
+    const editorRef = useRef(null);
+    const editorDom = useRef(null);
 
-  useEffect(() => {
-    if (editorRef.current) return
-    editorRef.current = new EditorView(editorDom.current, {
-      state: EditorState.create({ doc, plugins }),
-    })
-  }, [])
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <div id="editor" ref={editorDom} />
+    useEffect(() => {
+        if (editorRef.current) return;
+        editorRef.current = new EditorView(editorDom.current, {
+            state: EditorState.create({ doc, plugins })
+        });
+    }, []);
+
+    return <div id="editor" ref={editorDom} />;
 }
 
-export default Editor
+export default Editor;
