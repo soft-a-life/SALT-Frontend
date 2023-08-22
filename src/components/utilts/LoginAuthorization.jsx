f334aac7b67142369638152191c75df8a837c3a1
kimyougmin <dudals896@gmail.com>
2023-08-22 18:46:43 +0900

    <feat> Decryption and encryption code for written text

--- src/components/utilts/LoginAuthorization.jsx
+++ src/components/utilts/LoginAuthorization.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
 import { useCookies } from 'react-cookie'
 
-function LoginAuthorization(props) {
-  const [cookies, setCookie, removeCookie] = useCookies(['loginToken'])
+function LoginAuthorization() {
+  const [cookies, setCookie] = useCookies(['accessToken', 'refreshToken'])
   try {
     fetch('http://localhost:8080/accounts/refreshToken', {
       method: 'POST',
       headers: {
+        refresh: cookies.refreshToken,
         'Content-Type': 'application/json; charset=utf-8',
       },
-      body: JSON.stringify(cookies.loginToken),
+      body: {},
     })
+      .then((res) => res.json())
       .then((res) => {
-        return true
+        return setCookie('accessToken', res.ACCESS_TOKEN, {
+          httpOnly: true,
+        })
       })
       .catch((e) => {
-        return false
+        return alert('세션이 만료되었습니다!')
       })
   } catch (e) {
     alert(e)
