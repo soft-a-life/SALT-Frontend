b4a3dfbdb0109611b44a02b57d6473402d3b701e
kimyougmin <dudals896@gmail.com>
2023-08-24 17:02:30 +0900

    <feat> Add image insertion function to editor
    이미지는 크기 등은 2000X800 추후에 들어온 크기에 따라 변하게 개선해야함

--- src/postWriteScreen/WEditor.tsx
+++ src/postWriteScreen/WEditor.tsx
@@ -1,43 +1,77 @@
-import React, { useCallback, useState } from 'react'
-import type { RemirrorJSON } from 'remirror'
-import { OnChangeJSON } from '@remirror/react'
-import { WysiwygEditor } from '@remirror/react-editors/wysiwyg'
-import { ImageExtension } from 'remirror/extensions'
+import 'remirror/styles/all.css'
 
+import React, { useCallback } from 'react'
+import {
+  ApplySchemaAttributes,
+  NodeExtensionSpec,
+  NodeSpecOverride,
+  RemirrorJSON,
+} from 'remirror'
+import { ImageExtension } from 'remirror/extensions'
+import {
+  OnChangeJSON,
+  Remirror,
+  ThemeProvider,
+  useRemirror,
+} from '@remirror/react'
+
+class FigcaptionExtension extends ImageExtension {
+  createNodeSpec(
+    extra: ApplySchemaAttributes,
+    override: NodeSpecOverride,
+  ): NodeExtensionSpec {
+    const spec = super.createNodeSpec(extra, override)
+
+    return {
+      ...spec,
+      attrs: {
+        ...spec.attrs,
+        figcaptionText: { default: '' },
+      },
+      toDOM: (node) => [
+        'figure',
+        {
+          style:
+            'border: 2px solid #479e0c; padding: 8px; margin: 8px; text-align: center;',
+        },
+        spec.toDOM!(node),
+        [
+          'figcaption',
+          { style: 'background-color: #3d3d3d; color: #f1f1f1; padding: 8px;' },
+          node.attrs.figcaptionText,
+        ],
+      ],
+    }
+  }
+}
+
+const extensions = () => [new FigcaptionExtension()]
 const STORAGE_KEY = 'remirror-editor-content'
 
-const WEditor: React.FC = () => {
-  const [initialContent] = useState<RemirrorJSON | undefined>(() => {
-    // Retrieve the JSON from localStorage (or undefined if not found)
-    const content = localStorage.getItem(STORAGE_KEY)
-    return content ? JSON.parse(content) : undefined
+const WEditor = (): JSX.Element => {
+  const { manager, state, onChange } = useRemirror({
+    extensions,
   })
-
   const handleEditorChange = useCallback((json: RemirrorJSON) => {
-    // Store the JSON in localStorage
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(json))
+    // Store the JSON in localstorage
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(json))
   }, [])
-
   return (
-    <MyEditor onChange={handleEditorChange} initialContent={initialContent} />
-  )
-}
-
-interface MyEditorProps {
-  onChange: (json: RemirrorJSON) => void
-  initialContent?: RemirrorJSON
-}
-
-const MyEditor: React.FC<MyEditorProps> = ({ onChange, initialContent }) => {
-  return (
-    <div style={{ padding: 16 }}>
-      <WysiwygEditor
-        placeholder="Enter text..."
-        initialContent={initialContent}
+    <ThemeProvider>
+      <Remirror
+        manager={manager}
+        autoFocus
+        onChange={onChange}
+        initialContent={state}
+        autoRender="end"
       >
-        <OnChangeJSON onChange={onChange} />
-      </WysiwygEditor>
-    </div>
+        <OnChangeJSON
+          onChange={(e) => {
+            handleEditorChange(e)
+          }}
+        />
+      </Remirror>
+    </ThemeProvider>
   )
 }
 
