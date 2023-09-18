3c6867158617cbf41e1ce4642c0344df1ee06e15
kimyougmin <dudals896@gmail.com>
2023-09-18 09:41:59 +0900

    prosemirror image update
    이미지가 동시에 여러장이 들어왔을 경우 혹은 이미지가 여러장 추가 될 경우 어떻게 처리할 지

--- src/postWriteScreen/WEditor.tsx
+++ src/postWriteScreen/WEditor.tsx
@@ -15,46 +15,53 @@ import {
   useRemirror,
 } from '@remirror/react'
 
+interface ProseMirrorJSON {
+  type: string
+  content?: ProseMirrorJSON[]
+  src?: string
+  alt?: string
+}
 class FigcaptionExtension extends ImageExtension {
   createNodeSpec(
     extra: ApplySchemaAttributes,
     override: NodeSpecOverride,
   ): NodeExtensionSpec {
     const spec = super.createNodeSpec(extra, override)
-
     return {
       ...spec,
       attrs: {
         ...spec.attrs,
         figcaptionText: { default: '' },
       },
-      toDOM: (node) => [
-        'figure',
-        {
-          style:
-            'border: 2px solid #479e0c; padding: 8px; margin: 8px; text-align: center;',
-        },
-        spec.toDOM!(node),
-        [
-          'figcaption',
-          { style: 'background-color: #3d3d3d; color: #f1f1f1; padding: 8px;' },
-          node.attrs.figcaptionText,
-        ],
-      ],
     }
   }
 }
+function extractImageNodes(data: ProseMirrorJSON[]): ProseMirrorJSON[] {
+  const imageNodes: ProseMirrorJSON[] = []
 
+  data.forEach((node) => {
+    if (node.type === 'image') {
+      imageNodes.push(node)
+      console.log('node.type=', node)
+    }
+    if (node.content) {
+      const childImageNodes = extractImageNodes(node.content)
+      imageNodes.push(...childImageNodes)
+    }
+  })
+  return imageNodes
+}
 const extensions = () => [new FigcaptionExtension()]
 const STORAGE_KEY = 'remirror-editor-content'
 
-const WEditor = (): JSX.Element => {
+const WEditor = (): React.JSX.Element => {
   const { manager, state, onChange } = useRemirror({
     extensions,
   })
   const handleEditorChange = useCallback((json: RemirrorJSON) => {
-    // Store the JSON in localstorage
-    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(json))
+    const imageNodes = extractImageNodes([json])
+    console.log('imageNodes', imageNodes)
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(json))
   }, [])
   return (
     <ThemeProvider>
